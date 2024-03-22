ef47988c1d700011159081beefc718ab0d1b8123
eunsun <eunsunjj9501@gmail.com>
2024-03-22 13:09:36 +0900

    Create Account API - Error

--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,7 +1,13 @@
 package com.example.account.domain;
 
+import com.example.account.type.AccountStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 
 @Getter
@@ -10,14 +16,27 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Account {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @ManyToOne
+    private AccountUser accountUser;
     private String accountNumber;
 
     @Enumerated(EnumType.STRING)
     private AccountStatus accountStatus;
+    private Long balance;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unRegisteredAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updateAt;
 }
+
--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -1,48 +1,97 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.account.type.AccountStatus;
+import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
-    @Autowired
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
     private AccountService accountService;
 
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testXXX() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+
+        //when
+        Account account = accountService.getAccount(4555L);
+
+        //then
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(4555L, captor.getValue());
+        assertNotEquals(45515L, captor.getValue());
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void testFailedToSearchAccount() {
+        //given
+        //when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+
+        //then
+        assertEquals("Minus", exception.getMessage());
     }
 
     @Test
     @DisplayName("Test 이름 변경")
     void testGetAccount() {
-        Account account = accountService.getAccount(1L);
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
 
-        assertEquals("40000", account.getAccountNumber());
-        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+        //when
+        Account account = accountService.getAccount(4555L);
 
+        //then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
 
     @Test
     void testGetAccount2() {
-        Account account = accountService.getAccount(2L);
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
 
-        assertEquals("40000", account.getAccountNumber());
-        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+        //when
+        Account account = accountService.getAccount(4555L);
 
+        //then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
-
-//    @Test
-//    void testSomething() {
-//        String something = "Hello " + "World";
-//
-//        assertEquals("Hello World", something);
-//    }
 }
