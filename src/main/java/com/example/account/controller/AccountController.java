869e5a79b8778c5e3850b48859f8548a83754d68
eunsun <eunsunjj9501@gmail.com>
2024-03-25 17:28:29 +0900

    Avoid duplicate transactions (AOP 활용)

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -1,16 +1,12 @@
 package com.example.account.controller;
 
 import com.example.account.domain.Account;
-import com.example.account.dto.AccountDto;
 import com.example.account.dto.AccountInfo;
 import com.example.account.dto.CreateAccount;
 import com.example.account.dto.DeleteAccount;
-import com.example.account.repository.AccountRepository;
 import com.example.account.service.AccountService;
-import com.example.account.service.RedisTestService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Delete;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +16,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
-    private final RedisTestService redisTestService;
 
     @PostMapping("/account") // 계좌 생성 API
     public CreateAccount.Response createAccount(
@@ -56,11 +51,6 @@ public class AccountController {
                         .build())
                 .collect(Collectors.toList());
 
-    }
-
-    @GetMapping("/get-lock")
-    public String getLock() {
-        return redisTestService.getLock();
     }
 
     @GetMapping("/account/{id}")
--- src/main/java/com/example/account/dto/UseBalance.java
+++ src/main/java/com/example/account/dto/UseBalance.java
@@ -1,5 +1,6 @@
 package com.example.account.dto;
 
+import com.example.account.aop.AccountLockIdInterface;
 import com.example.account.type.TransactionResultType;
 import io.lettuce.core.TransactionResult;
 import jakarta.validation.constraints.*;
@@ -19,7 +20,7 @@ public class UseBalance {
     @Getter
     @Setter
     @AllArgsConstructor
-    public static class Request {
+    public static class Request implements AccountLockIdInterface {
         @NotNull
         @Min(1)
         private Long userId;
